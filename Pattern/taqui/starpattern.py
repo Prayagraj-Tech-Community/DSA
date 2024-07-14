3035db35c013b22253586e770810178a677d0489
Md Taqui Ansari <142133106+taquiansari@users.noreply.github.com>
2024-07-14 19:20:23 +0530

    Create starpattern.py

---
+++ Pattern/taqui/starpattern.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+for i in range(4,-1,-1):
+    for j in range(i,-1,-1):
+        print("*",end=" ")
+    print()   
